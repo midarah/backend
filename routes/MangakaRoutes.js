6a1d9708dde04a8cdea108abf23519fca9693918
midarah <midara.superuser@outlook.com>
2023-10-15 04:01:25 -0300

    Primeiro commit

---
+++ routes/MangakaRoutes.js
@@ -0,0 +1,12 @@
+import MangakaController from "../controllers/MangakaController.js";
+import { Router } from "express";
+const router = Router();
+// Middlewares
+import verifyToken from "../helpers/verify-token.js";
+import { imageUpload } from "../helpers/image-upload.js";
+// Rotas
+router.post("/create", verifyToken, imageUpload.single("image"), MangakaController.create);
+router.get("/", MangakaController.getAllMangakas);
+router.get("/:id", MangakaController.getMangakaById);
+router.delete("/:id", verifyToken, MangakaController.deleteMangaka);
+export default router;
